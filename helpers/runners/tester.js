1eac6ee6dec9a7a0de5d76daba8ccb1d5b4ee6b9
TomoBossi <tomasbossi97@gmail.com>
2024-09-14 10:25:14 -0300

    fix parsing of functions and imports (fr)

--- helpers/runners/tester.js
+++ helpers/runners/tester.js
@@ -9,28 +9,31 @@ let tester = new CodeRunner({
 });
 
 export const preTestPrologue = `\n
-RESERVEDIMPORTNAMEARRAY = ["RESERVEDINSPECTEMPTY", "RESERVEDIMPORTNAMEARRAY", "RESERVEDINSPECTSIGNATURE", "RESERVEDINSPECTGETFILE", "RESERVEDINSPECTISMODULE", "RESERVEDINSPECTGETMODULE", "RESERVEDOUTPUTDICTIONARY"]
+RESERVEDIMPORTNAMEARRAY = ["sys", "io", "RESERVEDINSPECTEMPTY", "RESERVEDIMPORTNAMEARRAY", "RESERVEDINSPECTSIGNATURE", "RESERVEDINSPECTGETFILE", "RESERVEDINSPECTISMODULE", "RESERVEDINSPECTGETMODULE", "RESERVEDOUTPUTDICTIONARY"]
 from inspect import Parameter as RESERVEDINSPECTEMPTY, signature as RESERVEDINSPECTSIGNATURE, getfile as RESERVEDINSPECTGETFILE, ismodule as RESERVEDINSPECTISMODULE, getmodule as RESERVEDINSPECTGETMODULE
 RESERVEDINSPECTEMPTY = RESERVEDINSPECTEMPTY.empty
 \n`
 
 export const preTestEpilogue = `\n
-RESERVEDOUTPUTDICTIONARY = {"local_functions": {}, "partial_imports": {}, "true_imports": []}
+RESERVEDOUTPUTDICTIONARY = {"local_functions": {}, "imports": {"partial_imports": {}, "true_imports": []}}
 for (name, obj) in [(k, v) for k, v in locals().items()]:
   if name not in RESERVEDIMPORTNAMEARRAY:
     if callable(obj):
-      if RESERVEDINSPECTGETFILE(obj) == "<exec>":
-        RESERVEDOUTPUTDICTIONARY["local_functions"][name] = {
-          "parameters": [parameter for parameter, _ in RESERVEDINSPECTSIGNATURE(obj).parameters.items()], 
-          "parameters_literal": str(RESERVEDINSPECTSIGNATURE(obj))
-        }
-      else:
-        if RESERVEDINSPECTGETMODULE(obj).__name__ in RESERVEDOUTPUTDICTIONARY["partial_imports"]:
-          RESERVEDOUTPUTDICTIONARY["partial_imports"][RESERVEDINSPECTGETMODULE(obj).__name__].append(f"{obj.__name__} as {name}")
+      try:
+        if RESERVEDINSPECTGETFILE(obj) == "<exec>":
+          RESERVEDOUTPUTDICTIONARY["local_functions"][name] = {
+            "parameters": [parameter for parameter, _ in RESERVEDINSPECTSIGNATURE(obj).parameters.items()], 
+            "parameters_literal": str(RESERVEDINSPECTSIGNATURE(obj))
+          }
         else:
-          RESERVEDOUTPUTDICTIONARY["partial_imports"][RESERVEDINSPECTGETMODULE(obj).__name__] = [f"{obj.__name__} as {name}"]
-    elif RESERVEDINSPECTISMODULE(obj) and name not in ("sys", "io"):
-      RESERVEDOUTPUTDICTIONARY["true_imports"].append(f"{obj.__name__} as {name}")
+          raise(TypeError)
+      except:
+        if RESERVEDINSPECTGETMODULE(obj).__name__ in RESERVEDOUTPUTDICTIONARY["imports"]["partial_imports"]:
+          RESERVEDOUTPUTDICTIONARY["imports"]["partial_imports"][RESERVEDINSPECTGETMODULE(obj).__name__].append(f"{obj.__name__} as {name}")
+        else:
+          RESERVEDOUTPUTDICTIONARY["imports"]["partial_imports"][RESERVEDINSPECTGETMODULE(obj).__name__] = [f"{obj.__name__} as {name}"]
+    elif RESERVEDINSPECTISMODULE(obj):
+      RESERVEDOUTPUTDICTIONARY["imports"]["true_imports"].append(f"{obj.__name__} as {name}")
 sys.stdout = io.StringIO()
 print(RESERVEDOUTPUTDICTIONARY)
 \n`
